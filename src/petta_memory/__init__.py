0956e22043aef3e0c5000976fce29aed268b64be
Benjamin Goertzel <ben@singularitynet.io>
2026-09-06 01:05:22 -0700

    feat: export ECAN + WMTM classes from package __init__
    All ECAN (AttentionValue, AttentionBank, ImportanceDiffusion, RentCollection,
    ECANCycle, ECANCycleResult, ECANBridge) and WMTM (WMTMItem, ForgettingPolicy,
    WMTMStore, RecallBridge, WMTMUtility, WMTMInferenceEngine, WMTMCoordinator)
    classes are now importable directly from petta_memory.
    
    Total __all__ exports: 22 (was 8).

--- src/petta_memory/__init__.py
+++ src/petta_memory/__init__.py
@@ -5,13 +5,44 @@ from .petta_runtime import make_petta_parse_checker
 from .store import MediumMemoryStore, MemoryCluster, ValidationError
 from .usability_bundle import validate_provider_free_usability_bundle
 
+# ECAN attention allocation
+from .ecan import AttentionValue, AttentionBank, ImportanceDiffusion, RentCollection, ECANCycle, ECANCycleResult
+from .ecan_bridge import ECANBridge
+
+# Working Memory with Tick-driven Maintenance
+from .wmtm import WMTMItem, ForgettingPolicy, WMTMStore
+from .wmtm_recall import RecallBridge
+from .wmtm_utility import WMTMUtility
+from .wmtm_inference import WMTMInferenceEngine
+from .wmtm_coordinator import WMTMCoordinator
+
 __all__ = [
-    "LiveWriteDisabled",
+    # Core store
     "MediumMemoryStore",
     "MemoryCluster",
-    "OmegaClawMemoryBridge",
-    "make_petta_parse_checker",
-    "OmegaClawMemoryPolicy",
     "ValidationError",
+    # OmegaClaw bridge
+    "OmegaClawMemoryBridge",
+    "OmegaClawMemoryPolicy",
+    "LiveWriteDisabled",
+    # Runtime
+    "make_petta_parse_checker",
+    # Usability
     "validate_provider_free_usability_bundle",
+    # ECAN
+    "AttentionValue",
+    "AttentionBank",
+    "ImportanceDiffusion",
+    "RentCollection",
+    "ECANCycle",
+    "ECANCycleResult",
+    "ECANBridge",
+    # WMTM
+    "WMTMItem",
+    "ForgettingPolicy",
+    "WMTMStore",
+    "RecallBridge",
+    "WMTMUtility",
+    "WMTMInferenceEngine",
+    "WMTMCoordinator",
 ]
